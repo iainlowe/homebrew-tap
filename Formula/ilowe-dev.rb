a8a5f137ae8325618929fadb2d13ddc599fb42cc
Iain Lowe <iain.lowe@gmail.com>
2022-11-05 17:53:00 -0400

    Update ilowe-dev.rb

--- Formula/ilowe-dev.rb
+++ Formula/ilowe-dev.rb
@@ -2,6 +2,6 @@ class IloweDev < Formula
   version "0.1alpha1"
   sha256 "71d66e87a9561f8cc70f06a466a5f75a77aa9cb55e8795e0539c514eff7cf7d3"
   url "https://img.spacergif.org/v1/spacer.gif"
-  depends_on "nmap"
+  depends_on "nmap go git 1password-cli"
   depends_on :cask "osxfuse"
 end
